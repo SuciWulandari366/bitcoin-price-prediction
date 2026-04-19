f6bf2fed5b846c331da3efc07ec45feb843308ff
SuciWulandari366 <pramitasuci632@gmail.com>
2026-04-19 18:15:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import lightgbm as lgb
 model = lgb.Booster(model_file='model_bitcoin (5).txt')
 
 st.title("Prediksi Harga Bitcoin 🪙")
-st.write("Masukkan data hari ini untuk memprediksi harga Bitcoin besok")
 
 open_price = st.number_input("Open Price")
 high = st.number_input("High")
@@ -16,10 +15,10 @@ close_lag1 = st.number_input("Close Hari Sebelumnya")
 if st.button("Prediksi"):
     data = pd.DataFrame([[open_price, high, low, volume, close_lag1]],
                         columns=['Open', 'High', 'Low', 'Volume', 'Close_lag1'])
-    
-    # Samakan urutan fitur
-    data = data[model.feature_name()]
-    
+
+    # Debug
+    st.write("Fitur model:", model.feature_name())
+
     pred = model.predict(data)
     
     st.success(f"Prediksi Harga Bitcoin Besok: {pred[0]:,.2f}")
